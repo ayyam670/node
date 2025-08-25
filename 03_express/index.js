a3c66d2636a6a1aebc9fbd6b39d6cb76287814b6
HJW <ayyam670@naver.com>
2025-08-25 17:47:59 +0900

    0825 수업

--- 03_express/index.js
+++ 03_express/index.js
@@ -13,6 +13,16 @@ app.get("/start", (req, resp)=>
   resp.send("/start 경로 호출됨.");
 });
 
+app.get("/json", (req, resp)=>
+{
+  resp.send({id: "user01", pw: "1111"});
+});
+
+app.post("/main", (req,resp) =>
+{
+  resp.send("/main 경로를 post 요청방식으로 호출함");
+});
+
 // 서버스타트
 app.listen(3000, () =>
 {
