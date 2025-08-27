738627e72c73fd307b3c8735e2329b53a49f1434
HJW <ayyam670@naver.com>
2025-08-27 14:51:43 +0900

    0827 수업중

--- 04_mysql/app.js
+++ 04_mysql/app.js
@@ -1,19 +1,7 @@
 // app.js
 const express = require("express");
-const mysql = require("mysql2");
 const parser = require("body-parser");
-
-
-// connect pool 생성.
-const pool = mysql.createPool(
-{
-  host: "127.0.0.1", //= localhost
-  port: 3306,
-  user: "dev01",
-  password: "dev01",
-  database: "dev",
-  connectionLimit: 10,
-});
+const sql = require("./sql");
 
 
 const app = express();
@@ -27,93 +15,70 @@ app.get("/", (req, resp) =>
 });
 
 // 고객목록.
-app.get("/customers", (req, resp) =>
+app.get("/customers", async(req, resp) =>
 {
-  //connection = pool.getConnection((err, connetion) =>
-  pool.getConnection((err, connection) =>
+  try{
+  let customerList = await sql.execute("select * from customers");
+  console.log(customerList);
+  resp.json(customerList); // 웹페이지에 출력
+  }
+  catch(err)
   {
-    // getConnection => connection 객체 획득
-    if(err)
-    {
-      console.log(err);
-      return;
-    }
-  
-    connection.query("select * from customers", (err, results) =>
-    {
-      if(err)
-      {
-        console.log(err);
-        resp.send("쿼리 실행 중 에러");
-        return;
-      }
-      console.log(results);
-      //resp.send("실행완료.");
-      resp.json(results);
-      connection.release(); // connection -> pool 환원.
-    }); // end query().
-  }); // end getConnection().
+    console.log(err);
+    resp.json({retCode: "Error"});
+  }
 });
 
 // 등록
-app.post("/customer", (req, resp) =>
+app.post("/customer", async(req, resp) =>
 {
   console.log(req.body.param);
-    pool.getConnection((err, connection) =>
+    try
     {
-      if(err)
-      {
-        console.log(err);
-        return;
-      }
-    
-      connection.query(//"insert into customers (name, email, phone) values (?,?,?)", [req.body.name, req.body.email, req.body.phone],
-                      "insert into customers set ?", [req.body.param], // [{name: "방재우", email: "bang@email.com", phone: "010-1111-7777"}]
-      (err, results) =>
-      {
-        if(err)
-        {
-          console.log(err);
-          resp.send("쿼리 실행 중 에러");
-          return;
-        }
-        console.log(results);
-        //resp.send("실행완료.");
-        resp.json(results);
-        connection.release(); // connection -> pool 환원.
-      }); // end query().
-    }); // end getConnetion().
+      let result = await sql.execute("insert into customers set ?", [req.body.param]);
+      console.log(result);
+      resp.json(result); // 웹페이지에 출력
+    }
+    catch(err)
+    {
+      console.log(err);
+      resp.json({retCode: "Error"});
+    }
   });
 
 // http://localhost:8080/boardList.do?page=3
 // http://localhost:3000/customer/:id
-app.delete("/customer/:id", (req,resp) =>
+app.delete("/customer/:id", async(req,resp) =>
 {
   console.log(req.params.id);
-  pool.getConnection((err, connection) =>
+  try
   {
-    // getConnection => connection 객체 획득
-    if(err)
-    {
-      console.log(err);
-      return;
-    }
-  
-    connection.query("delete from customers where id = ?", Number(req.params.id) ,
-    (err, results) =>
-    {
-      if(err)
-      {
-        console.log(err);
-        resp.send("쿼리 실행 중 에러");
-        return;
-      }
-      console.log(results);
-      //resp.send("실행완료.");
-      resp.json(results);
-      connection.release(); // connection -> pool 환원.
-    }); // end query().
-  }); // end getConnection().
+    let remove = await sql.execute("delete from customers where id=?", [req.params.id]);
+    console.log(remove);
+    resp.json(remove); // 웹페이지에 출력
+  }
+  catch(err)
+  {
+    console.log(err);
+    resp.json({retCode: "Error"});
+  }
+  resp.send("OK");
+});
+
+app.put("/customer/:id", async(req,resp) =>
+{
+  console.log(req.params.id);
+  try
+  {
+    let modify = await sql.execute("update customers set ? where id=?", [req.body.param, req.params.id]);
+    console.log(modify);
+    resp.json(modify); // 웹페이지에 출력
+  }
+  catch(err)
+  {
+    console.log(err);
+    resp.json({retCode: "Error"});
+  }
   resp.send("OK");
 });
 
